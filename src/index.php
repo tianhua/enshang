266f1782c29ec65dfd82853bfcd62a0cd4d70c95
tianhua <azheng@SI-AZHENG.corp.valueclick.com>
2014-02-21 17:12:45 +0800

    add struct

--- src/index.php
+++ src/index.php
@@ -2,6 +2,7 @@
 <html>
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<script type='text/javascript' src='assets/js/jquery.js'></script>
 <title>Insert title here</title>
 </head>
 <body>
